441705b2b2475094f99527c1248a5d9f22c0e85f
riconokvip <ricon@okvip.com>
2023-11-01 23:26:51 +0700

    update get s3

--- src/App.js
+++ src/App.js
@@ -1,39 +1,13 @@
-import React, { useState } from 'react';
-import s3 from './service/s3';
+import React from 'react';
+import { getListBuckets, uploadObject } from './service/s3';
 
 export default function App() {
-  const [imageFile, setImageFile] = useState(null);
 
-  console.log(process.env.ACCESS_KEY_ID)
+	console.log(process.env.ACCESS_KEY_ID)
 
-  const handleImageChange = (e) => {
-	if (e.target.files && e.target.files[0]) {
-	  const blob = e.target.files[0];
-	  const params = { Body: blob, 
-					   Bucket: "luongsonchatapp", 
-					   Key: blob.name};
-	   // Sending the file to the Spaces
-	   	s3.putObject(params)
-		 .on('build', request => {
-		   request.httpRequest.headers.Host = "https://luongsonchatapp.sgp1.digitaloceanspaces.com";
-		   request.httpRequest.headers['Content-Length'] = blob.size;
-		   request.httpRequest.headers['Content-Type'] = blob.type;
-		   request.httpRequest.headers['x-amz-acl'] = 'public-read';
-		})
-		.send((err) => {
-		  if (err) {console.log(err)}
-		  else {
-		  // If there is no error updating the editor with the imageUrl
-		  const imageUrl = "https://luongsonchatapp.sgp1.digitaloceanspaces.com/" + blob.name
-		  console.log(imageUrl, blob.name)
-		 }
-	  });
-	}
-  };
-
-  return (
-    <div>
-      <input type="file" onChange={handleImageChange} />
-    </div>
-  );
+	return (
+		<div>
+			<input type="file" onChange={getListBuckets} />
+		</div>
+	);
 };
--- src/service/s3.js
+++ src/service/s3.js
@@ -1,13 +1,55 @@
-import AWS from 'aws-sdk';
+// Step 1: Import the S3Client object and all necessary SDK commands.
+import { ListBucketsCommand, ListObjectsCommand, PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
 
-/**
- * Digital Ocean Spaces Connection
- */
 
-const spacesEndpoint = new AWS.Endpoint('sgp1.digitaloceanspaces.com');
-const s3 = new AWS.S3({
-      endpoint: spacesEndpoint,
-      accessKeyId: "DO00D8CQ4MA2EQKTMNYQ",
-      secretAccessKey: "GTsDEYSqYXWDbIEIYxsRWgoYWNIl4t/dR9EZBYjmz2I"
-    });
-export default s3;
+// Step 2: The s3Client function validates your request and directs it to your Space's specified endpoint using the AWS SDK.
+const s3Client = new S3Client({
+    endpoint: "https://sgp1.digitaloceanspaces.com", // Find your endpoint in the control panel, under Settings. Prepend "https://".
+    forcePathStyle: false, // Configures to use subdomain/virtual calling format.
+    region: "sgp1", // Must be "us-east-1" when creating new Spaces. Otherwise, use the region in your endpoint (e.g. nyc3).
+    credentials: {
+      accessKeyId: "DO00D8CQ4MA2EQKTMNYQ", // Access key pair. You can create access key pairs using the control panel or API.
+      secretAccessKey: "GTsDEYSqYXWDbIEIYxsRWgoYWNIl4t/dR9EZBYjmz2I" // Secret access key defined through an environment variable.
+    }
+});
+
+
+// Step 3: Define the parameters for the object you want to upload.
+const params = {
+  Bucket: "luongsonchatapp", // The path to the directory you want to upload the object to, starting with your Space name.
+  Key: "test", // Object key, referenced whenever you want to access this file later.
+  Body: "/9j/4AAQSkZJRgABAQEASABIAAD/2wBDAAQDAwQDAwQEAwQFBAQFBgoHBgYGBg0JCggKDw0QEA8NDw4RExgUERIXEg4PFRwVFxkZGxsbEBQdHx0aHxgaGxr/2wBDAQQFBQYFBgwHBwwaEQ8RGhoaGhoaGhoaGhoaGhoaGhoaGhoaGhoaGhoaGhoaGhoaGhoaGhoaGhoaGhoaGhoaGhr/wAARCAAyADIDASIAAhEBAxEB/8QAHAAAAQUBAQEAAAAAAAAAAAAAAAIDBAUGAQcI/8QAKBAAAgEDAgUDBQAAAAAAAAAAAAECAwQREiEFEzFBURWBoRQiQmFi/8QAGQEAAwEBAQAAAAAAAAAAAAAAAwQFAgYA/8QAHhEAAgEEAwEAAAAAAAAAAAAAAAECAwQRMRITFCH/2gAMAwEAAhEDEQA/APq3VsNVJrDGeesdSLcXKSe5ASMOSSIfE7pRi9zGcRvnmWGXHFbrOrcx17UbkxmCI9zVGa9w3nLIVSplhPPcbcW+wysIlNtnNX7A7y5eAPZRj6eteobdyLcX2U9yiV28dRMq7fclqRadfKF3dZzyU1ai5vYsd5sfpWil+OTXdxFnF1GUMbGUn0HFw9+M+xqaXD00tvgfXDfKAyuwsbRmQ+g/n4A2Hpq8MAfrN+QzWoNYy5idW4dREkyyoLL6F5Z0E0ihtJbo0llNKKEq7a0UreKeyfSopLoSFRXgRTmsD8ZZJcmyvFITyV4QDuQB5ZvCPKZdjqADqUckibaPdF9Zt6WACVbZSty2oP7fYmQ6gBKnsrQ0PAAAQh//2Q==", // The object's contents. This variable is an object, not a string.
+  ACL: "public", // Defines ACL permissions, such as private or public.
+  
+  Metadata: { // Defines metadata tags.
+    "x-amz-meta-my-key": "your-value"
+  }
+};
+
+
+// Step 4: Define a function that uploads your object using SDK's PutObjectCommand object and catches any errors.
+export const uploadObject = async () => {
+  try {
+    const data = await s3Client.send(new PutObjectCommand(params));
+    console.log(
+      "Successfully uploaded object: " +
+        params.Bucket +
+        "/" +
+        params.Key
+    );
+    console.log(data);
+  } catch (err) {
+    console.log("Error", err);
+  }
+};
+
+
+export const getListBuckets = async () => {
+  try {
+    const data = await s3Client.send(new ListObjectsCommand({ Bucket: "luongsonchatapp" }));
+    console.log("Success", data);
+    return data; // For unit tests.
+  } catch (err) {
+    console.log("Error", err);
+  }
+};
